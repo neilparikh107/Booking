cdecab15cea46dd6e518dc25c6cbe8b05868b1ae
neilparikh107 <nparikh@cozero.com.au>
2016-11-11 03:30:01 +1100

    second  commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :displays
   resources :customers
-  root to: 'customers#index'
+  root to: 'bookings#new'
   resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
